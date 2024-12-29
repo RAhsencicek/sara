c41eb30a4b2359189204f113113dd79c696faa67
RAhsencicek <ahsen.cicek752@gmail.com>
2024-12-29 23:13:30 +0300

    Initial Commit

---
+++ sa-ra-(saferange)/sa_ra__saferange_App.swift
@@ -0,0 +1,17 @@
+//
+//  sa_ra__saferange_App.swift
+//  sa-ra-(saferange)
+//
+//  Created by Ahsen on 29.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct sa_ra__saferange_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
